aec903a4109c0fa5ce200249a58992002f2ffaf9
otto <nick.otto@deepgram.com>
2026-02-11 14:42:59 -0800

    Optimizing build so this doesn't take as long every time

--- deepclaw_deploy.sh
+++ deepclaw_deploy.sh
@@ -1,4 +1,28 @@
-#fly deploy . -a deepclaw-instance --config fly.deepclaw.toml --build-only
-IMAGE=$(fly deploy . --buildkit -a deepclaw-instance --config fly.deepclaw.toml 2>&1 | tee /dev/stderr | grep '^image:' | cut -d\  -f2)
-echo $IMAGE
-fly -y -a deepclaw-instance --config fly.deepclaw.toml scale count 0
+tag=$(git describe --always)
+IMAGE="registry.fly.io/deepclaw-instance:$tag"
+
+echo "Building $IMAGE"
+docker build -t $IMAGE -f Dockerfile.fly .
+echo "Pushing $IMAGE"
+docker push $IMAGE
+
+echo "\n $IMAGE \n"
+
+
+echo "Deploying"
+
+# Make sure control plane is running
+curl https://deepclaw-control.fly.dev/api/health
+
+echo "Truncating DB"
+printf "TRUNCATE users, machines, api_keys, pool_machines CASCADE;\n\\q\n" | fly postgres connect -a deepclaw-db -d deepclaw_control -
+
+# curl -s -H "Authorization: Bearer $DEEPCLAW_LITELLM_MASTER_KEY" https://deepclaw-litellm.fly.dev/key/list | jq '{keys: [.[].token]}' | curl -s -X POST -H "Authorization: Bearer $DEEPCLAW_LITELLM_MASTER_KEY" -H "Content-Type: application/json" -d @- https://deepclaw-litellm.fly.dev/key/delete
+
+fly machines list -a deepclaw-instance --json | jq -r '.[].id' | xargs -I{} fly machines destroy {} -a deepclaw-instance --force
+
+
+
+curl https://deepclaw-control.fly.dev/api/health
+
+fly secrets set "OPENCLAW_IMAGE=$IMAGE" -a deepclaw-control
